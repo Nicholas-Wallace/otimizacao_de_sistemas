92913fa9fc64c1f131117b03357a3fad80f01326
Nicholas Wallace <stuffedpotato11@gmail.com>
2026-05-13 10:45:12 -0300

    simplex iteration working

--- simplex.jl
+++ simplex.jl
@@ -9,28 +9,27 @@ function simplex_iteration(M::Matrix{T}, basicas::Vector{Int}; max=true) where T
     
     # Step - Verificar se a solução é otima
     if max
-        if M[1, :] .>= 0
+        if all(M[1, :] .>= 0)
             println("Solução ótima encontrada!")
             return M
-        end
-
-    else
-        # caso contrario econtrar a variavel que deve entrar na base
-        new_basic = argmin(M[1,1:(end-1)])
-
-        # e encontrar a variavel que deve sair da base (a que mais limitar seu crescimento)
-        ratios = M[2:end, :] ./ M[2:end, new_basic]
-
-        if min(ratios) <= 0
-            println("Solução ótima encontrada!")
-            return M
-        
         else
-            leaving_basic = argmin(ratios)
-            println("Nova variável básica: x$new_basic, Variável que sai da base: x$(basicas[leaving_basic])")
-            basicas[leaving_basic] = new_basic
+            println("estamos no else")
+            # caso contrario econtrar a variavel que deve entrar na base
+            new_basic = argmin(M[1,1:(end-1)])
+
+            # e encontrar a variavel que deve sair da base (a que mais limitar seu crescimento)
+            ratios = M[2:end, new_basic] ./ M[2:end, end]
+            @show ratios
+            if maximum(ratios) <= 0
+                println("Solução ótima encontrada!")
+                return M
+            
+            else
+                leaving_basic = argmin(ratios)
+                println("Nova variável básica: x$new_basic, Variável que sai da base: x$(basicas[leaving_basic])")
+                basicas[leaving_basic] = new_basic
+                return M, basicas
+            end
         end
-
-
     end
 end
